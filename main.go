27dc17e0d7951246ef4f90a2707625b499604da5
Lizy <LizyZZZ@163.com>
2023-11-28 00:56:01 +0800

    learn database/sql

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"database/sql"
+	"fmt"
 
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -13,14 +14,49 @@ type User struct {
 }
 
 func main() {
-	db, err := sql.Open("mysql", "root:123456@(localhost:3306)/webserver")
+	db, err := sql.Open("mysql", "root:123456@(127.0.0.1:3306)/webserver")
 	if err != nil {
 		panic(err)
 	}
+	defer db.Close()
 
-	row := db.QueryRowContext(context.Background(), "SELECT username, password FROM user WHERE id = 1")
+	id := 1
+	row := db.QueryRowContext(context.Background(), "SELECT username, password FROM user WHERE id = ?;", id)
 	if row.Err() != nil {
+		if row.Err() == sql.ErrNoRows {
+			fmt.Println(row.Err())
+		} else {
+			panic(row.Err())
+		}
+	}
+
+	// 解析结果
+	var user User
+	if err = row.Scan(&user.Username, &user.Password); err != nil {
 		panic(err)
+	}
+	fmt.Println(user)
+
+	rows, err := db.QueryContext(context.Background(), "SELECT * FROM user;")
+	if err != nil {
+		if err == sql.ErrNoRows {
+			fmt.Println(err)
+		} else {
+			panic(err)
+		}
+	}
+
+	for rows.Next() {
+		var ID int32
+		var name string
+		var pwd string
+		if err := rows.Scan(&ID, &name, &pwd); err != nil {
+			panic(err)
+		}
+		fmt.Println(ID, name, pwd)
+	}
+	if rows.Err() != nil {
+		panic(rows.Err())
 	}
 
 }
